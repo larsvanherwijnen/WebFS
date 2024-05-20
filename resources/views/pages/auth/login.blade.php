446bdca24d1e1e3219290ad0b217ffdd2d4ccb0c
Lars van Herwijnen <lars.druten@gmail.com>
2024-05-20 13:51:48 +0200

    Fix small bug

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.auth')
 
-@section('title', __('auth.login'))
+@section('title', 'Inloggen')
 
 @section('content')
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0 w-full">
