8181cb332fbf136646dbaa7d54a41ae9bcd8e23f
Lars van Herwijnen <lars.druten@gmail.com>
2024-05-25 15:10:41 +0200

    Update menu

--- resources/views/partials/cashier/menu.blade.php
+++ resources/views/partials/cashier/menu.blade.php
@@ -1,46 +1,21 @@
-<aside
-    class="bg-white shadow-sm -translate-x-80 fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0 border border-blue-gray-100">
-    <div class="relative"><a class="py-6 px-8 text-center" href="{{ route('admin.checkout') }}"><h6
-                class="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-blue-gray-900">
-                De Goudedraak</h6></a>
+<div class="py-4">
+    <div class="px-4 flex items-center justify-between mb-4">
+        <div class="text-xl font-bold">Gouden draak</div>
     </div>
-    <div class="m-4">
-        <ul class="mb-4 flex flex-col gap-1">
-            <li>
-                <a aria-current="page" class="active" href="{{ route('admin.checkout') }}">
-                    <button
-                        class="align-middle select-none font-sans font-bold text-center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] w-full flex items-center gap-4 px-4 capitalize"
-                        type="button" control-id="ControlID-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                        </svg>
-                        <p class="block antialiased font-sans text-base leading-relaxed text-inherit font-medium capitalize">Kassa</p>
-                    </button>
-                </a>
+    <div class="border-b border-gray-200">
+        <ul class="px-4 space-x-4 flex flex-wrap text-md font-medium text-gray-500">
+            <li class="me-2 flex">
+                <a href="{{ route('admin.checkout') }}" aria-current="page"
+                   class="{{ request()->routeIs('admin.checkout') ? 'border-b-2 border-black text-black font-bold' : 'pb-2' }}">Dashboard</a>
             </li>
-            <li>
-                <a  href="{{ route('admin.sales') }}">
-                    <button
-                        class="align-middle select-none font-sans font-bold text-center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 rounded-lg text-blue-gray-500 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30 w-full flex items-center gap-4 px-4 capitalize"
-                        type="button" control-id="ControlID-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 7.756a4.5 4.5 0 1 0 0 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                        </svg>
-                        <p class="block antialiased font-sans text-base leading-relaxed text-inherit font-medium capitalize">Verkoop overzicht</p>
-                    </button>
-                </a></li>
-            <li>
-                <a href="{{ route('logout') }}">
-                    <button
-                        class="align-middle select-none font-sans font-bold text-center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 rounded-lg text-blue-gray-500 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30 w-full flex items-center gap-4 px-4 capitalize"
-                        type="button" control-id="ControlID-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
-                        </svg>
-                        <p class="block antialiased font-sans text-base leading-relaxed text-inherit font-medium capitalize">uitloggen</p>
-                    </button>
-                </a>
+            <li class="me-2 flex">
+                <a href="{{ route('admin.sales') }}"
+                   class="{{ request()->routeIs('admin.sales') ? 'border-b-2 border-black text-black font-bold' : 'pb-2' }}">Verkoop overzicht</a>
+            </li>
+            <li class="me-2 flex">
+                <a href="{{ route('logout') }}"
+                   class="{{ request()->routeIs('logout') ? 'border-b-2 border-black text-black font-bold' : 'pb-2' }}">Uitloggen</a>
             </li>
         </ul>
     </div>
-</aside>
+</div>
