6fc9422d06c1e48a39ebafc03e585022087c8617
Lars van Herwijnen <lars.druten@gmail.com>
2024-06-14 14:34:18 +0200

    Add navigation buttons

--- resources/views/livewire/create-dish.blade.php
+++ resources/views/livewire/create-dish.blade.php
@@ -1,9 +1,14 @@
 <div>
-    <form wire:submit="create">
-        {{ $this->form }}
+    <div class="space-y-3">
+        <a href="{{route('admin.dishes')}}" class="bg-green-500 rounded p-2 mb-4">
+            terug naar overzicht
+        </a>
+        <form wire:submit="create">
+            {{ $this->form }}
 
-        <button type="submit" class="bg-green-500 rounded p-2 mt-4">
-            Opslaan
-        </button>
-    </form>
+            <button type="submit" class="bg-green-500 rounded p-2 mt-4">
+                Opslaan
+            </button>
+        </form>
+    </div>
 </div>
--- resources/views/livewire/update-dish.blade.php
+++ resources/views/livewire/update-dish.blade.php
@@ -1,9 +1,14 @@
 <div>
-    <form wire:submit="update">
-        {{ $this->form }}
+    <div class="space-y-3">
+        <a href="{{route('admin.dishes')}}" class="bg-green-500 rounded p-2 mb-4">
+            terug naar overzicht
+        </a>
+        <form wire:submit="update">
+            {{ $this->form }}
 
-        <button type="submit" class="bg-green-500 rounded p-2 mt-4">
-            Aanpassing opslaan
-        </button>
-    </form>
+            <button type="submit" class="bg-green-500 rounded p-2 mt-4">
+                Aanpassing opslaan
+            </button>
+        </form>
+    </div>
 </div>
