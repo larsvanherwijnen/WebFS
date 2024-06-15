81d4a1bc815c0dbc1592e90968ac0e3c79dfa2e1
Mike de Groot <mike251105@gmail.com>
2024-06-15 12:30:33 +0200

    🚧 Original layout

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,21 +1,24 @@
-<!-- <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-</html>
+<!doctype html>
+<html>
 
 <head>
-    <title>{{ __('The Golden Dragon') }}</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title>The Golden Dragon</title>
     <style>
         body {
             background-color: darkred;
-            margin: 15px 50px;
+            margin: 15px;
+            margin-left: 50px;
+            margin-right: 50px
+        }
+
+        td {
+            padding: 0px;
         }
 
         @font-face {
             font-family: 'chinese_takeawayregular';
-            src: url('/fonts/chinesetakeaway-webfont.woff2') format('woff2'),
-                url('/fonts/chinesetakeaway-webfont.woff') format('woff');
+            src: url('fonts/chinesetakeaway-webfont.woff2') format('woff2'),
+                url('fonts/chinesetakeaway-webfont.woff') format('woff');
             font-weight: normal;
             font-style: normal;
         }
@@ -25,104 +28,216 @@
             color: yellow;
         }
     </style>
-</head> -->
-@extends('layouts.app')
+</head>
 
+<body>
+    <table id="main_table" style="padding:5px;width:100%;border-collapse: collapse">
+        <tr style="height:50px;background-color:red">
+            <td style="text-align:center;width:30%;color:yellow;font-size:30px">
+                <img style="vertical-align: middle;" src="/images/dragon-small.png" alt="Golden Dragon" height="50px">
+                <span style="font-family:'chinese_takeawayregular'">De Gouden Draak</span>
+                <img style="vertical-align: middle;" src="/images/dragon-small-flipped.png" alt="Golden Dragon"
+                    height="50px">
+            </td>
+            <td>
+                <a href="paginas/aanbiedingen.html" style="color:yellow;font-weight:bold;text-decoration: none;">
+                    <marquee behavior="scroll" direction="left">
+                        Welkom bij De Gouden Draak. Klik op deze tekst om de aanbiedingen van deze week te zien!
+                    </marquee>
+                </a>
+            </td>
+            <td style="text-align:center;width:30%;color:yellow;font-size:30px">
+                <img style="vertical-align: middle;" src="/images/dragon-small.png" alt="Golden Dragon" height="50px">
+                <span style="font-family:'chinese_takeawayregular'">De Gouden Draak</span>
+                <img style="vertical-align: middle;" src="/images/dragon-small-flipped.png" alt="Golden Dragon"
+                    height="50px">
+            </td>
+        </tr>
+    </table>
+    <table id="main_table" style="padding:5px;width:100%;border-collapse: collapse">
+        <tr style="height:7px;background-color:red">
+            <td colspan="9">
+            </td>
+        <tr>
+        <tr style="height:25px;background-color:red">
+            <td width="7px">
+            </td>
+            <td style="width:25px;border-left:4px solid yellow;border-top:4px solid yellow"></td>
+            <td style="width:25px;border-right:4px solid yellow;border-top:4px solid yellow"></td>
+            <td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
+            <td style="border-top:4px solid yellow;border-bottom:4px solid yellow"></td>
+            <td style="width:25px;border-left:4px solid yellow;border-bottom:4px solid yellow"></td>
+            <td style="width:25px;border-left:4px solid yellow;border-top:4px solid yellow"></td>
+            <td style="width:25px;border-right:4px solid yellow;border-top:4px solid yellow"></td>
+            <td width="7px">
+        </tr>
+        <tr style="height:25px;background-color:red">
+            <td width="7px">
+            <td style="width:25px;border-left:4px solid yellow;border-bottom:4px solid yellow"></td>
+            <td style="width:25px;border:4px solid yellow"></td>
+            <td style="width:25px;border:4px solid yellow"></td>
+            <td></td>
+            <td style="width:25px;border:4px solid yellow"></td>
+            <td style="width:25px;border:4px solid yellow"></td>
+            <td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
+            <td width="7px">
+        </tr>
+        <tr style="height:25px;background-color:red">
+            <td width="7px">
+            <td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
+            <td style="width:25px;border:4px solid yellow"></td>
+            <td style="width:25px"></td>
+            <td></td>
+            <td style="width:25px"></td>
+            <td style="width:25px;border:4px solid yellow"></td>
+            <td style="width:25px;border-bottom:4px solid yellow"></td>
+            <td width="7px">
+        </tr>
+        <tr style="height:50px;background-color:red">
+            <td width="7px">
+            <td style="width:25px;border-right:4px solid yellow;border-left:4px solid yellow"></td>
+            <td style="width:25px;"></td>
+            <td style="width:25px;"></td>
+            <td style="text-align:center">
+                <!-- CONTENT HERE! -->
+                <table width=100%>
+                    <tr>
+                        <td colspan='3'>
+                            <p>
+                                <img src="/images/dragon-small.png" style="float:left;height:200px" alt="Golden Dragon">
+                                <img src="/images/dragon-small-flipped.png" style="float:right;height:200px"
+                                    alt="Golden Dragon">
+                                <span style="font-size:40px;font-weight:bold;color:yellow">Chinees Indische
+                                    Specialiteiten</span><br>
+                                <span style="font-size:50px;font-weight:bold;color:yellow">De Gouden Draak</span><br>
+                            </p>
+                            <p>
+                            <table style="margin:auto;font-size:20px;color:white" border="1px solid white">
+                                <tr background="/images/menu_bg_gradient.png">
+                                    <td valign="middle">
+                                        <a href="paginas/MENUKAART.html" style="color:white">
+                                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Menukaart&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                        </a>
+                                    </td>
+                                    <td valign="middle">
+                                        <a href="paginas/news.html" style="color:white">
+                                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nieuws&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                        </a>
+                                    </td>
+                                    <td valign="middle">
+                                        <a href="paginas/contact.html" style="color:white">
+                                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contact&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                        </a>
+                                    </td>
+                                </tr>
+                            </table>
+                            </p>
+                        </td>
+                    </tr>
+                    <tr style="padding-top:50px">
+                        <td colspan="3" height="50px">
+                        </td>
+                    </tr>
+                    <tr style="padding-top:50px">
+                        <td width="50px">
+                        </td>
+                        <td align="center" style='font-size:5;border:1px solid black;background:floralwhite'> <br>
+                            <h3>Al jaren is De Gouden Draak een begrip als het gaat om de beste afhaalgerechten in
+                                's-Hertogenbosch.<br>
+                                Graag trakteren we u op authentieke gerechten uit de Cantonese keuken.</h3>
+                            <br>
+                            <h2><u>Speciale Studentenaanbieding</h2></u>
+                            <h1>Chinese Rijsttafel (2 personen)</h1>
+                            <h3>
+                                Maak een keuze uit 3 van onderstaande keuzegerechten:<br><br>
+                                <table width="60%">
+                                    <tr>
+                                        <td width="40%" style="text-align:right">
+                                            Koe Loe Yuk
+                                        </td>
+                                        <td width="20%">
+                                        </td>
+                                        <td width="40%">
+                                            Foe Yong Hai
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="text-align:right">
+                                            Tjap Tjoy
+                                        </td>
+                                        <td>
+                                        </td>
+                                        <td>
+                                            Garnalen met Gebakken Knoflook
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="text-align:right">
+                                            Babi Pangang
+                                        </td>
+                                        <td>
+                                        </td>
+                                        <td>
+                                            Kipfilet in Zwarte Bonen saus
+                                        </td>
+                                    </tr>
+                                </table>
+                                <br>
+                                Met witte rijst. (Nasi of bami voor meerprijs mogelijk.)
+                            </h3>
+                            <h1>Prijs: €21,00</h1>
+                        </td>
+                        <td width="50px">
+                        </td>
+                    </tr>
+                </table>
+                <br>
+                <div text-align="center"><a href="paginas/contact_new.html">Naar Contact</a></div>
+            </td>
+            <td style="width:25px;"></td>
+            <td style="width:25px;"></td>
+            <td style="width:25px;border-right:4px solid yellow;border-left:4px solid yellow"></td>
+            <td width="7px">
+        </tr>
+        <tr style="height:25px;background-color:red">
+            <td width="7px">
+            <td style="width:25px;border-right:4px solid yellow;border-top:4px solid yellow"></td>
+            <td style="width:25px;border:4px solid yellow"></td>
+            <td style="width:25px"></td>
+            <td></td>
+            <td style="width:25px"></td>
+            <td style="width:25px;border:4px solid yellow"></td>
+            <td style="width:25px;border-top:4px solid yellow"></td>
+            <td width="7px">
+        </tr>
+        <tr style="height:25px;background-color:red">
+            <td width="7px">
+            <td style="width:25px;border-left:4px solid yellow;border-top:4px solid yellow"></td>
+            <td style="width:25px;border:4px solid yellow"></td>
+            <td style="width:25px;border:4px solid yellow"></td>
+            <td></td>
+            <td style="width:25px;border:4px solid yellow"></td>
+            <td style="width:25px;border:4px solid yellow"></td>
+            <td style="width:25px;border-right:4px solid yellow;border-top:4px solid yellow"></td>
+            <td width="7px">
+        </tr>
+        <tr style="height:25px;background-color:red">
+            <td width="7px">
+            </td>
+            <td style="width:25px;border-left:4px solid yellow;border-bottom:4px solid yellow"></td>
+            <td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
+            <td style="width:25px;border-right:4px solid yellow"></td>
+            <td style="border-top:4px solid yellow;border-bottom:4px solid yellow"></td>
+            <td style="width:25px;border-left:4px solid yellow;"></td>
+            <td style="width:25px;border-left:4px solid yellow;border-bottom:4px solid yellow"></td>
+            <td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
+            <td width="7px">
+        </tr>
+        <tr style="height:7px;background-color:red">
+            <td colspan="9">
+            </td>
+        <tr>
+    </table>
+</body>
 
-{{ session('locale', App::getLocale()) }}
-{{__('global.test')}}
-@section('content')
-    <body>
-    <div class="flex items-center justify-between bg-red-700 py-3 px-5">
-        <div class="flex items-center text-yellow-500 text-2xl">
-            <img class="h-12" src="/images/dragon-small.png" alt="Golden Dragon">
-            <span class="font-chinese_takeawayregular ml-2">{{ __('global.test') }}</span>
-            <img class="h-12 ml-2" src="/images/dragon-small-flipped.png" alt="Golden Dragon">
-        </div>
-        <a href="paginas/aanbiedingen.html" class="font-bold">
-            <marquee behavior="scroll" direction="left">
-                {{ __('Welkom bij De Gouden Draak. Klik op deze tekst om de aanbiedingen van deze week te zien!') }}
-            </marquee>
-        </a>
-        <div class="flex items-center text-yellow-500 text-2xl">
-            <img class="h-12" src="/images/dragon-small.png" alt="Golden Dragon">
-            <span class="font-chinese_takeawayregular ml-2">{{ __('De Gouden Draak') }}</span>
-            <img class="h-12 ml-2" src="/images/dragon-small-flipped.png" alt="Golden Dragon">
-        </div>
-    </div>
-
-    <div class="flex justify-center bg-red-700 py-1">
-        <div class="flex w-full max-w-5xl border-t-4 border-b-4 border-l-4 border-r-4 border-yellow-500"></div>
-    </div>
-
-    <div class="flex justify-center bg-red-700 py-1">
-        <div class="flex w-full max-w-5xl border-4 border-yellow-500"></div>
-    </div>
-
-    <div class="flex justify-center bg-red-700 py-1">
-        <div class="flex w-full max-w-5xl border-4 border-yellow-500"></div>
-    </div>
-
-    <div class="flex justify-center bg-red-700 py-3">
-        <div class="w-full max-w-5xl text-center text-yellow-500">
-            <img class="float-left h-48" src="/images/dragon-small.png" alt="Golden Dragon">
-            <img class="float-right h-48" src="/images/dragon-small-flipped.png" alt="Golden Dragon">
-            <h1 class="text-4xl font-bold">{{ __('Chinees Indische Specialiteiten') }}</h1>
-            <h2 class="text-5xl font-bold">{{ __('De Gouden Draak') }}</h2>
-            <div class="flex justify-center mt-5">
-                <a href="paginas/MENUKAART.html"
-                   class="mx-2 text-xl text-white bg-red-800 py-2 px-4 rounded-lg">{{ __('Menukaart') }}</a>
-                <a href="paginas/news.html"
-                   class="mx-2 text-xl text-white bg-red-800 py-2 px-4 rounded-lg">{{ __('Nieuws') }}</a>
-                <a href="paginas/contact.html"
-                   class="mx-2 text-xl text-white bg-red-800 py-2 px-4 rounded-lg">{{ __('Contact') }}</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="flex justify-center bg-red-700 py-10">
-        <div class="w-full max-w-4xl text-center bg-floralwhite border border-black p-5 rounded-lg bg-white">
-            <h3 class="text-2xl">
-                {{ __('Al jaren is De Gouden Draak een begrip als het gaat om de beste afhaalgerechten in \'s-Hertogenbosch. Graag trakteren we u op authentieke gerechten uit de Cantonese keuken.') }}
-            </h3>
-            <h2 class="text-3xl font-bold underline mt-5">{{ __('Speciale Studentenaanbieding') }}</h2>
-            <h1 class="text-4xl font-bold">{{ __('Chinese Rijsttafel (2 personen)') }}</h1>
-            <h3 class="text-2xl mt-5">
-                {{ __('Maak een keuze uit 3 van onderstaande keuzegerechten:') }}
-                <div class="flex justify-around mt-5 text-xl">
-                    <div>{{ __('Koe Loe Yuk') }}</div>
-                    <div>{{ __('Foe Yong Hai') }}</div>
-                </div>
-                <div class="flex justify-around mt-2 text-xl">
-                    <div>{{ __('Tjap Tjoy') }}</div>
-                    <div>{{ __('Garnalen met Gebakken Knoflook') }}</div>
-                </div>
-                <div class="flex justify-around mt-2 text-xl">
-                    <div>{{ __('Babi Pangang') }}</div>
-                    <div>{{ __('Kipfilet in Zwarte Bonen saus') }}</div>
-                </div>
-                <div class="mt-5">{{ __('Met witte rijst. (Nasi of bami voor meerprijs mogelijk.)') }}</div>
-            </h3>
-            <h1 class="text-4xl font-bold mt-5">{{ __('Prijs: €21,00') }}</h1>
-        </div>
-    </div>
-
-    <div class="flex justify-center py-5">
-        <a href="paginas/contact_new.html" class="text-yellow-500 text-xl">{{ __('Naar Contact') }}</a>
-    </div>
-
-    <div class="flex justify-center py-5">
-        <!-- call a post request to the route with name 'language.change' and add a button with the nl parameter and the en parameter in the route -->
-        <!-- Make sure that the route has the "locale" parameter -->
-
-        <form action="{{ route('language.change') }}" method="POST" class="bg-green-500">
-            @csrf
-            <button type="submit" name="locale" value="nl"
-                    class="text-yellow-500 text-xl">{{ __('Nederlands') }}</button>
-            <button type="submit" name="locale" value="en" class="text-yellow-500 text-xl">{{ __('English') }}</button>
-        </form>
-    </div>
-    </body>
-
-    </html>
-@endsection
+</html>
